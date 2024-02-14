f8718c7b3167c6fa349aac73af3f7f217bd1eab7
Paul Tibbetts <paul.tibbetts@stickee.co.uk>
2024-02-14 14:25:17 +0000

    feat: use rector v1 config

--- dist/rector.php
+++ dist/rector.php
@@ -5,30 +5,27 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use RectorLaravel\Set\LaravelSetList;
 use RectorLaravel\Set\LaravelLevelSetList;
-use Rector\Set\ValueObject\SetList;
-use Rector\Set\ValueObject\LevelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/app'
-    ]);
-
-    $rectorConfig->sets([
-        SetList::DEAD_CODE,
-        SetList::CODE_QUALITY,
-        SetList::CODING_STYLE,
-        SetList::TYPE_DECLARATION,
-        LevelSetList::UP_TO_PHP_81,
+    ])
+    ->withPhpSets() // picks up version from composer.json
+    ->withPreparedSets(
+        codeQuality: true,
+        codingStyle: true,
+        deadCode: true,
+        typeDeclarations: true,
+    )
+    ->withSets([
         LaravelSetList::LARAVEL_CODE_QUALITY,
         LaravelLevelSetList::UP_TO_LARAVEL_90,
-    ]);
-
-    $rectorConfig->skip([
+    ])
+    ->withSkip([
         \Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector::class, // removes @param from docblocks
         \Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector::class, // removes return from docblocks
         \Rector\CodingStyle\Rector\PostInc\PostIncDecToPreIncDecRector::class, // changes $i++ to ++$i
         \Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector::class, // changes "th{$is}" to sprintf('th%s', 'is')
         \Rector\Php80\Rector\FunctionLike\MixedTypeRector::class, // removes docblocks
-    ]);
-
-};
+    ])
+;
