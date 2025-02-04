4f6bd0f48142714872e7b78772813b7c7a1bc589
Paul Tibbetts <paul.tibbetts@stickee.co.uk>
2025-02-04 14:57:02 +0000

    chore: fix path in add generic return type to relations rector

--- dist/add-generic-return-type-to-relations.php
+++ dist/add-generic-return-type-to-relations.php
@@ -4,7 +4,6 @@ use Rector\Config\RectorConfig;
 
 /** https://gist.github.com/canvural/7385ec70d2719e9961886430fbb4798c */
 return RectorConfig::configure()
-    ->withPaths([__DIR__ . '/app'])
     ->withRules([
         \RectorLaravel\Rector\ClassMethod\AddGenericReturnTypeToRelationsRector::class,
     ]);
